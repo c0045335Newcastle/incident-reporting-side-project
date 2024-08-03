de2be6b82d9ef263283aae1531cbf6cac7e780a5
Sam Dickinson <c0045335@newcastle.ac.uk>
2024-08-03 18:26:22 +0100

    More on server.js foundation

--- server.js
+++ server.js
@@ -17,4 +17,39 @@ app.post('/register', async (req, res) => {
     // push is just like append to end of array - returns array length
     users.push({username, password:hashedPassword});
     res.send({message:'User registered successfully.'});
-})
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find(u  => u.username === username);
+    if (user && await bcrypt.compare(password, user.password)) {
+        const token = jwt.sign({username}, JWT_SECRET, {expiresIn: '1h'});
+        res.send({token})
+    }else{
+        res.status(401).send({message:'Invalid username or password'});
+    }
+});
+
+app.post('/incident',(req, res) => {
+    const {title, description} = req.body;
+    incidents.push({title, description});
+    res.send({message:'Incident registered successfully.'});
+});
+
+app.post('/dashboard',(req, res) => {
+    const authHeader = req.headers.authorization;
+
+    const token = authHeader && authHeader.authorization.split(' ')[1];
+    // get the auth token from the header
+    // format: Authorization: Bearer thisIsTheToken
+    if(!token) return res.status(401).send({message:'No token'});
+    jwt.verify(token, JWT_SECRET, (err, user) => {
+        if (err) return res.status(401).send({message:'Invalid token'});
+        res.send({incidents})
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
+
